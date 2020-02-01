f130b1341221db5226c6e2ca6508db41d459204c
tashif-hoda <tashif.hoda@gmail.com>
2020-02-01 17:14:57 +0530

    added coin change

---
+++ coin_change.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr = [0 for i in range(n+1)]
+arr[0]=1
+seq=[3, 5, 10]
+for x in seq:
+	for i in range(x, n+1):
+		if arr[i-x] != 0:
+			#arr[i]+=abs(arr[i]-arr[i-3])
+			arr[i]+=arr[i-x]
+
+print(arr[n])
