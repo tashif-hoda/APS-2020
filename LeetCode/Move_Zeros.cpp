816e78150958e4f5e1100bd538f186f361dfbd04
tashif-hoda <60363795+tashif-hoda@users.noreply.github.com>
2020-04-29 02:43:55 +0530

    Create Move_Zeros.cpp

---
+++ LeetCode/Move_Zeros.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector <int> stack;
+        int count=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[count++]=nums[i];
+            }
+        }
+        for(int i=count; i<nums.size();i++)
+        {
+            nums[i]=0;
+        }
+    }
+};
