07b129719911b57a2516da982700a86f916db09e
tashif-hoda <60363795+tashif-hoda@users.noreply.github.com>
2020-05-03 02:06:21 +0530

    Create goodland electricity.py

---
+++ goodland electricity.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+i,j,trans,flag,loc=0,0,0,0,0
+while i<n:
+    trans+=1
+    j=i+k-1
+    if j>n:
+        j=n-1
+    while loc<=j<n and a[j]==0:
+        j-=1
+    if j<loc:
+        print("-1 ")
+        flag=1
+        break
+    else:
+        loc=j+1
+        j+=k
+        i=j
+if flag==0:      
+    print(trans)
