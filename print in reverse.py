b4dcc71b08fc792f3e9c33cc3a51807e6af842af
tashif-hoda <60363795+tashif-hoda@users.noreply.github.com>
2020-05-06 01:38:56 +0530

    Create print in reverse.py

---
+++ print in reverse.py
@@ -0,0 +1,17 @@
+
+
+# Complete the reversePrint function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def reversePrint(head):
+    if head is not None:
+        reversePrint(head.next)
+        print(head.data)
+    return
